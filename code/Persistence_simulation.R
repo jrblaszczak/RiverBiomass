fc8e5c8d0770d95108c9c8c31c15d8602387452a
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2020-11-03 16:06:02 -0800

    Added persistence code simulation

--- code/Persistence_simulation.R
+++ code/Persistence_simulation.R
@@ -3,41 +3,50 @@
 # Simulate a oversimplified hydrograph
 Q <- rnorm(350, mean=10, sd=0.1)
 Q_peaks <- sample(1:350, 10)
-Q[Q_peaks] <- rnorm(length(Q_peaks), mean=11.5, sd=0.3)
+Q[Q_peaks] <- rnorm(length(Q_peaks), mean=11.5, sd=0.2)
 # Visualize oversimplified hydrograph
 plot(Q, ylim = c(9,12))
 
 # Create persistence vector P and set steepness of the curve (s) and critical discharge (c)
 P <- numeric(length(Q))
 P[1] <- 1
-s <- 10
+s <- 3
 c <- 11
-
 for(i in 2:length(Q)){
   P[i] = exp(-exp(s*(Q[i] - c)))
 }
+P #P is never 0 and therefore can serve as refuge biomass but cannot be modeled using a binomial fit
+
+# Repeat with a steeper and shallower curve (higher and lower s values)
+P_steeper <- numeric(length(Q)); P_shallower <- numeric(length(Q))
+P_steeper[1] <- 1; P_shallower[1] <- 1
+s_steep <- 10; s_shallow <- 1
+
+for(i in 2:length(Q)){
+  P_steeper[i] = exp(-exp(s_steep*(Q[i] - c)))
+  P_shallower[i] = exp(-exp(s_shallow*(Q[i] - c)))
+}
 
 ## Visualize
-df <- as.data.frame(cbind(Q,P))
-fit <- glm(df$P ~ df$Q, data=df, family = )
-newdat <- data.frame(Q=seq(min(df$Q), max(df$Q),len=350))
-newdat$P = predict(fit, newdata=newdat, type="response")
-plot(P ~ Q, data=df, col="red4")
-lines(P ~ Q, newdat, col="green4", lwd=2)
+library(ggplot2)
+library(cowplot)
 
+df <- as.data.frame(cbind(Q, P, P_steeper, P_shallower))
 
+plot_grid(
+  ggplot(df, aes(Q, P))+
+    geom_line(lwd=2)+
+    geom_line(aes(Q, P_steeper), color="purple",lwd=1)+
+    geom_line(aes(Q, P_shallower), color="red")+
+    theme_classic()+
+    scale_y_continuous(limits=c(0,1)),
+  
+  qplot(seq_along(Q), Q)+
+    geom_point(color="blue")+
+    theme_classic()+
+    geom_hline(yintercept = c), #horizontal line to represent critQ
+  
+  ncol=1)
 
-plot(Q, P, ylim = c(0,1))
-
-
-mtcars <- mtcars
-fit = glm(vs ~ hp, data=mtcars, family=binomial)
-predicted= predict(fit, newdata=mtcars, type="response")
-plot(vs~hp, data=mtcars, col="red4")
-lines(mtcars$hp, predicted, col="green4", lwd=2)
-
-
-
-
-
-
+# Notes: I tried a range of steeper s values than 10 and nothing budged.
+# The curve only seemed to change if the value was lower than the original s
