3be5f1a2c53730786110e68691562d97cd0a3243
jrblaszczak <joanna.r.blaszczak@gmail.com>
2021-02-10 19:09:04 -0800

    NHDplus attributes start

---
+++ code/NHDplus_RiverSelection.R
@@ -0,0 +1,22 @@
+## Link Appling sites with NHD
+
+install.packages("nhdplusTools", dependencies = T)
+
+## Load packages
+lapply(c("plyr","dplyr","ggplot2","cowplot","lubridate",
+         "tidyverse","data.table","sf","nhdplusTools"), require, character.only=T)
+
+  
+library(nhdplusTools)
+library(sf)
+
+
+
+
+
+
+
+
+
+
+
