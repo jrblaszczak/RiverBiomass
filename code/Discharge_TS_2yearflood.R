dd336a023b6c41dfea39af736b523c530d96255b
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2021-04-11 22:23:03 -0700

    Recurrence interval of 2 year flood

---
+++ code/Discharge_TS_2yearflood.R
@@ -0,0 +1,66 @@
+## Discharge Data Retrieval
+
+# load packages
+lapply(c("plyr","dplyr","ggplot2","cowplot","lubridate","parallel",
+         "tidyverse","rstan","bayesplot","shinystan","Metrics","MCMCglmm",
+         "reshape2","ggExtra","patchwork"), require, character.only=T)
+
+library(dataRetrieval)
+
+# Site numbers
+site_numbers <- c("01649500","02234000","03058000",
+                  "08180700","10129900","14211010")
+# Site information
+NWIS_Info <- lapply(site_numbers, function(x) readNWISsite(x))
+
+# Mean daily discharge
+parameterCd <- "00060"
+
+# Raw daily data
+rawDailyData <- lapply(site_numbers, function(y) readNWISdv(y,parameterCd,
+                                                            "1970-01-01","2020-12-31"))
+names(rawDailyData) <- site_numbers
+
+# Extract relevant information
+DailyQ <- lapply(rawDailyData, function(z) return(z[,c("site_no","Date","X_00060_00003","X_00060_00003_cd")]))
+
+# Remove provisional data
+DailyQ_clean <- lapply(DailyQ, function(x) return(x[which(x$X_00060_00003_cd %in% c("A","A e")),]))
+
+################################################
+## Calculate 2 year flood recurrence interval
+##############################################
+
+two_year_flood <- function(data){
+  
+  #Grouping maximum average daily discharge for each year
+  max.by.year<-data %>% group_by(year=floor_date(Date, "year")) %>% summarize(amount=max(X_00060_00003))
+  
+  #Recording the maximum discharges by year and removing N.A. values
+  maximas<-max.by.year$amount
+  maximas<-maximas[!is.na(maximas)]
+  
+  #Sorting maxima by decreasing order
+  sorted.maximas<-sort(maximas,decreasing=T)
+  # calculate rank for 2 year recurring flood
+  rank <- round((length(sorted.maximas)+1)/2, digits = 0)
+  # 2-year recurring flood
+  return(sorted.maximas[rank])
+
+}
+
+RI_two <- ldply(lapply(DailyQ_clean, function(y) two_year_flood(y)), data.frame)
+colnames(RI_two) <- c(".id","RI_2yr_Q")
+RI_two$site_name <- paste("nwis_",RI_two$.id, sep = "")
+RI_two <- RI_two[,c("site_name","RI_2yr_Q")]
+
+## Export - save to data folder
+write.csv(RI_two, "../data/RI_2yr_flood.csv")
+
+
+
+
+
+
+
+
