ed4cc47a804d6a21838c05f85e6e25d95a7b0eea
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2021-04-12 07:59:05 -0700

    r comparison

--- code/Biomass6_Fig_DensityDependence.R
+++ code/Biomass6_Fig_DensityDependence.R
@@ -36,24 +36,58 @@ stan_model_output_Ricker <- readRDS("./rds files/stan_9riv_output_Ricker_2021_03
 stan_model_output_Ricker <- stan_model_output_Ricker[names(df)]
 #stan_model_output_Gompertz <- readRDS("./rds files/stan_6riv_output_Gompertz.rds")
 
+## Extract parameters
+par_Ricker <- lapply(stan_model_output_Ricker, function(x) rstan::extract(x, c("r","lambda","s","c","B","P","pred_GPP","sig_p")))
+#par_Gompertz <- lapply(stan_model_output_Gompertz, function(x) rstan::extract(x, c("beta_0","beta_1","s","c","B","P","pred_GPP","sig_p")))
+
+#################################################
+## r and K distributions
+################################################
+
+r_K_func <- function(x) {
+  pars <- as.data.frame(cbind(x$r, x$lambda))
+  colnames(pars) <- c("r","lambda")
+  pars$K <- (-1*pars$r)/pars$lambda
+  return(pars) 
+}
+
+par_Ricker_rK <- ldply(lapply(par_Ricker, function(z) r_K_func(z)), data.frame)
+colnames(par_Ricker_rK)[which(colnames(par_Ricker_rK) == ".id")] <- "site_name"
+
+par_Ricker_rK$short_name <- revalue(as.character(par_Ricker_rK$site_name), replace = c("nwis_01649500"="Anacostia River, MD",
+                                                                                 "nwis_02234000"="St. John's River, FL",
+                                                                                 "nwis_03058000"="West Fork River, WV",
+                                                                                 "nwis_08180700"="Medina River, TX",
+                                                                                 "nwis_10129900"="Silver Creek, UT",
+                                                                                 "nwis_14211010"="Clackamas River, OR"))
 
 
+plot_grid(
 
+ggplot(par_Ricker_rK, aes(r, color=short_name, fill=short_name))+
+  geom_density(alpha=0.5)+
+  theme_bw()+labs(color="Site",fill="Site")+
+  theme(legend.position = c(0.7,0.7)),
 
+ggplot(par_Ricker_rK, aes(lambda, color=short_name, fill=short_name))+
+  geom_density(alpha=0.5)+
+  theme_bw()+labs(color="Site",fill="Site")+
+  theme(legend.position = "none")+
+  coord_cartesian(xlim = c(-0.15,0.15)),
 
+ggplot(par_Ricker_rK, aes(K, color=short_name, fill=short_name))+
+  geom_density(alpha=0.5)+
+  theme_bw()+labs(color="Site",fill="Site")+
+  theme(legend.position = "none")+
+  coord_cartesian(xlim = c(-500,500)),
 
-
-
+ncol=1, align = "hv")
 
 
 
 ##################################################
-## Extract and summarize parameters
+## Summarize parameters
 ###################################################
-## Extract and summarize parameters
-par_Ricker <- lapply(stan_model_output_Ricker, function(x) rstan::extract(x, c("r","lambda","s","c","B","P","pred_GPP","sig_p")))
-#par_Gompertz <- lapply(stan_model_output_Gompertz, function(x) rstan::extract(x, c("beta_0","beta_1","s","c","B","P","pred_GPP","sig_p")))
-
 
 ## Median parameter
 par_median <- function(par) {
