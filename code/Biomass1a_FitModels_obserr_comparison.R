fde1424d3863eacdbe9ecfb76731e56693f44862
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2021-04-18 11:19:16 -0700

    Observation error comparison

---
+++ code/Biomass1a_FitModels_obserr_comparison.R
@@ -0,0 +1,88 @@
+## Fitting models to data
+## JR Blaszczak
+
+# load packages
+lapply(c("plyr","dplyr","ggplot2","cowplot","lubridate",
+         "parallel","tidyverse","rstan","devtools",
+         "bayesplot","shinystan","Metrics","MCMCglmm","tictoc"), require, character.only=T)
+
+## Source data
+source("DataSource_9rivers.R")
+
+####################
+## Stan data prep ##
+####################
+rstan_options(auto_write=TRUE)
+options(mc.cores=6)#parallel::detectCores())
+
+stan_data_compile <- function(x){
+  data <- list(Ndays=length(x$GPP), light = x$light_rel, GPP = x$GPP,
+               GPP_sd = x$GPP_sd, tQ = x$tQ)
+  return(data)
+}
+
+stan_data_l <- lapply(df, function(x) stan_data_compile(x))
+
+#########################################
+## Run Stan to get parameter estimates - initial tests
+#########################################
+
+## Initial tests
+#AR
+test_ar <- stan("Stan_ProductivityModel1_Autoregressive_obserr.stan",
+             data=stan_data_l$nwis_08180700,
+             chains=3,iter=5000, control=list(max_treedepth=12))
+launch_shinystan(test_ar)
+
+#Ricker
+init_Ricker <- function(...) {
+  list(c = 0.5, s = 200)
+}
+test_ricker <- stan("Stan_ProductivityModel3_Ricker_fixedinit_obserr.stan",
+             data=stan_data_l$nwis_08180700,
+             init = init_Ricker,
+             chains=3,iter=2000, control=list(max_treedepth=12))
+launch_shinystan(test_ricker)
+
+
+#########################################
+## Run Stan to get parameter estimates - all sites
+#########################################
+
+## PM 1 - Phenomenological
+PM_outputlist_AR <- lapply(stan_data_l,
+                           function(x) rstan::stan("Stan_ProductivityModel1_Autoregressive.stan",
+                                                   data=x,chains=3,iter=5000, control=list(max_treedepth=12))) #5000 seconds
+PM_AR_elapsedtime <- lapply(PM_outputlist_AR, function(x) return(get_elapsed_time(x)))
+saveRDS(PM_outputlist_AR, "./rds files/stan_9riv_output_AR_2021_03_05.rds")
+saveRDS(PM_AR_elapsedtime, "./rds files/stan_9riv_AR_time_2021_03_05.rds")
+
+## PM 3 - Ricker
+init_Ricker <- function(...) {
+  list(c = 0.5, s = 200)
+}
+
+PM_outputlist_Ricker <- lapply(stan_data_l,
+                               function(x) stan("Stan_ProductivityModel3_Ricker_fixedinit.stan",
+                                                data=x,chains=3,iter=5000,init = init_Ricker,
+                                                control=list(max_treedepth=12)))
+PM_Ricker_elapsedtime <- lapply(PM_outputlist_Ricker, function(x) return(get_elapsed_time(x)))
+saveRDS(PM_outputlist_Ricker, "./rds files/stan_9riv_output_Ricker_2021_03_05.rds")
+saveRDS(PM_Ricker_elapsedtime, "./rds files/stan_9riv_Ricker_time_2021_03_05.rds")
+
+## PM 4 - Gompertz
+init_Gompertz <- function(...) {
+  list(c = 0.5, s = 200)
+}
+
+PM_outputlist_Gompertz <- lapply(stan_data_l,
+                                 function(x) stan("Stan_ProductivityModel5_Gompertz_fixedinit.stan",
+                                                  data=x,chains=3,iter=5000, 
+                                                  control=list(max_treedepth=12)))
+PM_Gompertz_elapsedtime <- lapply(PM_outputlist_Gompertz, function(x) return(get_elapsed_time(x)))
+saveRDS(PM_outputlist_Gompertz, "./rds files/stan_9riv_output_Gompertz_2021_03_05.rds")
+saveRDS(PM_Gompertz_elapsedtime, "./rds files/stan_9riv_Gompertz_time_2021_03_05.rds")
+
+
+
+
---
+++ code/Stan_ProductivityModel1_Autoregressive_obserr.stan
@@ -0,0 +1,48 @@
+
+  data {
+    int Ndays; // number of days
+    vector [Ndays] light; // relativized to max value; unitless
+    vector [Ndays] GPP; // estimates from posterior probability distributions; g O2 m-2 d-1
+    vector [Ndays] GPP_sd; // sd estimates from posterior probability distributions; g O2 m-2 d-1
+    vector [Ndays] tQ; // standardized discharge; unitless
+    }
+
+  parameters {
+    // Growth & Loss Parameters
+    real l_pred_GPP [Ndays]; // predicted GPP (log scale); g O2 m-2 d-1
+    real<lower=0> phi; // autoregressive coefficient; unitless
+    real<lower=0> alpha; // growth term; g O2 m-2 d-1
+    real<upper=0> beta; // discharge to loss conversion term; g O2 m-2 d-1
+  
+    // Error parameters
+    real<lower=0> sig_p; // sigma processes error
+    real<lower=0> sig_o; // sigma observation error
+  }
+
+
+  model {
+  
+  // Initial value
+  l_pred_GPP[1] ~ normal(log(GPP[1]), 0.1);
+  
+  // Process model
+  for (j in 2:(Ndays)) {
+    l_pred_GPP[j] ~ normal(phi*l_pred_GPP[j-1] + alpha*light[j] + beta*tQ[j], sig_p);
+  }
+  
+  // Observation model
+  for (j in 2:(Ndays)) {
+    GPP[j] ~ normal(exp(l_pred_GPP[j]), sig_o)T[0,];
+  }
+  
+  // Error priors
+  sig_p ~ normal(0,2)T[0,];
+  sig_o ~ normal(mean(GPP_sd), sd(GPP_sd))T[0,];
+  
+  // Param priors (weakly informative)  
+  phi ~ normal(0,1)T[0,];
+  alpha ~ normal(0,1)T[0,];
+  beta ~ normal(0,1)T[,0];
+  
+}
+
