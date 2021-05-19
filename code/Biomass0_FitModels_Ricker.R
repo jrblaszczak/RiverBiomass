45f96dad90b6feb8b4b488faff1061bfd8b668c4
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2021-05-19 10:56:14 -0700

    Ricker comparison

--- code/Biomass0_FitModels_Ricker.R
+++ code/Biomass0_FitModels_Ricker.R
@@ -55,6 +55,12 @@ PM_outputlist_Ricker_Gmod <- lapply(stan_data_l,
                                                 data=x,chains=3,iter=5000,init = init_Ricker,
                                                 control=list(max_treedepth=12)))
 
+list_comparison <- list(PM_outputlist_AR, PM_outputlist_Ricker,
+                        PM_outputlist_Ricker_Lmod, PM_outputlist_Ricker_Gmod)
+names(list_comparison) <- c("AR", "Ricker","Ricker_Lmod","Ricker_Gmod")
+
+saveRDS(list_comparison, "./rds files/Outputlist_all_comparison.rds")
+
 
 ###################################
 ## Visualize differences among models
@@ -334,7 +340,189 @@ plot_grid(
   ncol = 1)
 
 
+#######################
+## Compare parameter distributions
+#######################
 
+## Extract parameters
+par_Ricker <- lapply(PM_outputlist_Ricker, function(x) rstan::extract(x, c("r","lambda","s","c","B","P","pred_GPP","sig_p","sig_o")))
+par_Ricker_Lmod <- lapply(PM_outputlist_Ricker_Lmod, function(x) rstan::extract(x, c("r","lambda","alpha","s","c","B","P","pred_GPP","sig_p","sig_o")))
+par_Ricker_Gmod <- lapply(PM_outputlist_Ricker_Gmod, function(x) rstan::extract(x, c("r","lambda","alpha","s","c","B","P","pred_GPP","sig_p","sig_o")))
+
+## r and K distributions
+r_K_func <- function(x) {
+  rx <- x$r
+  rm <- as.data.frame(cbind(rx[1:2500],rx[2501:5000],rx[5001:7500]))
+  rm <- rm %>%
+    rowwise() %>% mutate(Avg=mean(c(V1, V2, V3))) 
+  
+  lx <- x$lambda
+  lambda <- as.data.frame(cbind(lx[1:2500],lx[2501:5000],lx[5001:7500]))
+  lambda <- lambda %>%
+    rowwise() %>% mutate(Avg=mean(c(V1, V2, V3))) 
+  
+  new <- as.data.frame(cbind(rm$Avg, lambda$Avg))
+  colnames(new) <- c("r","lambda")
+  new$K <- (-1*new$r)/new$lambda
+  
+  long <- gather(new)
+  
+  return(long)
+}
+
+
+rK <- ldply(lapply(par_Ricker, function(x) r_K_func(x)), data.frame)
+rK_Lmod <- ldply(lapply(par_Ricker_Lmod, function(x) r_K_func(x)), data.frame)
+rK_Gmod <- ldply(lapply(par_Ricker_Gmod, function(x) r_K_func(x)), data.frame)
+
+rK_adj <- function(x){
+  x$short_name <- revalue(as.character(x$.id), replace = c("nwis_07191222"="Beaty Creek, OK",
+                                                           "nwis_01608500"="South Br. Potomac River, WV"))
+  x$short_name <- factor(x$short_name, levels= site_order_list)
+  x$key <- factor(x$key, levels = c("r","lambda","K"))
+  return(x)
+
+}
+
+rK <- rK_adj(rK); rK_Lmod <- rK_adj(rK_Lmod); rK_Gmod <- rK_adj(rK_Gmod)
+
+rK$mod <- "LB-Ricker"
+rK_Lmod$mod <- "LB - Ricker light"
+rK_Gmod$mod <- "LB - Ricker GPP"
+
+rK_all <- rbind(rK, rK_Lmod, rK_Gmod)
+
+ggplot(rK_all, aes(value, fill = short_name))+
+  geom_histogram()+
+  facet_grid(mod ~ key, scales="free_x")+
+  theme_bw()
+
+
+#########################################
+## Model weights
+#########################################
+
+GPP_oos_preds <- function(preds, df, mean_mod, se_mod){
+  
+  simmat_list <- readRDS(preds)
+  simmat_list <- simmat_list[names(df)]
+  
+  # For every day extract median and CI
+  mean_simmat <- ldply(lapply(simmat_list, function(z) apply(z[[1]], 1, function(x) mean(x))), data.frame)
+  se_simmat <- ldply(lapply(simmat_list, function(z) apply(z[[1]], 1, function(x) sd(x))), data.frame)
+  
+  ## Plot simulated GPP
+  dat <- ldply(df, data.frame)
+  df_sim <- as.data.frame(cbind(dat$site_name, as.character(dat$date), 
+                                dat$GPP, dat$GPP_sd, 
+                                mean_simmat$X..i.., se_simmat$X..i..))
+  colnames(df_sim) <- c("site_name","Date","GPP","GPP_se", mean_mod, se_mod)
+  df_sim$Date <- as.POSIXct(as.character(df_sim$Date), format="%Y-%m-%d")
+  df_sim[,3:6] <- apply(df_sim[,3:6],2,function(x) as.numeric(as.character(x)))
+  df_sim[,3:6] <- log(df_sim[,3:6])
+  
+  ## Link to site_info
+  df_sim <- left_join(df_sim, site_info[,c("site_name","short_name")])
+  
+  return(df_sim)
+  
+}
+
+LB_oos <- GPP_oos_preds("./rds files/Ricker_oos_comparison.rds", dat_oos, "mean_p_GPP_LB", "se_p_GPP_LB")
+LB_oos_Lmod <- GPP_oos_preds("./rds files/Ricker_Lmod_oos_comparison.rds", dat_oos, "mean_p_GPP_LB_Lmod", "se_p_GPP_LB_Lmod")
+
+## Combine
+oos_preds <- merge(LB_oos, LB_oos_Lmod[,c("site_name","Date","mean_p_GPP_LB_Lmod", "se_p_GPP_LB_Lmod")])
+
+## Split by short_name
+oos_pl <- split(oos_preds, oos_preds$short_name)
+
+#######################
+## Calc weights
+########################
+
+visualize_support <- function(short.name){
+  
+  test <- oos_pl[[short.name]]
+  test$p_obs_LB_Lmod <- dnorm(test$GPP, test$mean_p_GPP_LB_Lmod, sqrt(test$GPP_se^2 + test$se_p_GPP_LB_Lmod^2), log = FALSE)
+  test$p_obs_LB <- dnorm(test$GPP, test$mean_p_GPP_LB, sqrt(test$GPP_se^2 + test$se_p_GPP_LB^2), log = FALSE)
+  
+  test2 <- test[-1,]
+  
+  #probability vectors
+  p_LB_Lmod <- c(0,test2$p_obs_LB_Lmod); p_LB <- c(0, test2$p_obs_LB)
+  #empty vectors
+  weight_LB_Lmod <- numeric(nrow(test2)+1)
+  weight_LB <- numeric(nrow(test2)+1)
+  #starting values
+  weight_LB_Lmod[1] <- 0.5; weight_LB[1] <- 0.5
+  
+  for(i in 2:length(weight_LB_Lmod)){
+    weight_LB_Lmod[i] <- (weight_LB_Lmod[(i-1)]*p_LB_Lmod[i])/((weight_LB_Lmod[(i-1)]*p_LB_Lmod[i])+(weight_LB[(i-1)]*p_LB[i]))
+    weight_LB[i] <- 1-weight_LB_Lmod[i]
+  }
+  support <- cbind(test2,"LB_Lmod_support" = weight_LB_Lmod[-1], "LB_support" = weight_LB[-1])
+  
+  ## visualize
+  theme_set(theme_bw())
+  
+  vis.plot <- plot_grid(
+    ggplot(support, aes(Date, exp(GPP)))+geom_point(color="grey75")+
+      geom_line(aes(Date, exp(mean_p_GPP_LB_Lmod)),color="purple",size=0.9)+
+      geom_ribbon(aes(Date, ymin=exp(mean_p_GPP_LB_Lmod) - exp(se_p_GPP_LB_Lmod),
+                      ymax=exp(mean_p_GPP_LB_Lmod) + exp(se_p_GPP_LB_Lmod)), fill="purple",alpha=0.3)+
+      geom_line(aes(Date, exp(mean_p_GPP_LB)),color="chartreuse4",size=0.9)+
+      geom_ribbon(aes(Date, ymin=exp(mean_p_GPP_LB) - exp(se_p_GPP_LB),
+                      ymax=exp(mean_p_GPP_LB) + exp(se_p_GPP_LB)), fill="chartreuse4",alpha=0.3)+
+      labs(y="GPP",title = short.name),
+    
+    ggplot(support, aes(Date, LB_Lmod_support))+geom_line(color="purple",size=0.9)+
+      geom_line(aes(Date, LB_support),color="chartreuse4",size=0.9)+
+      labs(y = "Model support", title = "Purple = Ricker light mod; Green = Ricker"),
+    ncol=1, align = "hv")
+  
+  return(vis.plot)
+  
+}
+
+plot_grid(
+visualize_support("Beaty Creek, OK"),
+visualize_support("South Br. Potomac River, WV"),
+ncol = 2)
+
+
+#############################
+## alpha vs. biomass
+#############################
+
+
+
+
+## r and K distributions
+r_K_func <- function(x) {
+  rx <- x$r
+  rm <- as.data.frame(cbind(rx[1:2500],rx[2501:5000],rx[5001:7500]))
+  rm <- rm %>%
+    rowwise() %>% mutate(Avg=mean(c(V1, V2, V3))) 
+  
+  lx <- x$lambda
+  lambda <- as.data.frame(cbind(lx[1:2500],lx[2501:5000],lx[5001:7500]))
+  lambda <- lambda %>%
+    rowwise() %>% mutate(Avg=mean(c(V1, V2, V3))) 
+  
+  new <- as.data.frame(cbind(rm$Avg, lambda$Avg))
+  colnames(new) <- c("r","lambda")
+  new$K <- (-1*new$r)/new$lambda
+  
+  long <- gather(new)
+  
+  return(long)
+}
+
+
+rK <- ldply(lapply(par_Ricker, function(x) r_K_func(x)), data.frame)
+rK_Lmod <- ldply(lapply(par_Ricker_Lmod, function(x) r_K_func(x)), data.frame)
+rK_Gmod <- ldply(lapply(par_Ricker_Gmod, function(x) r_K_func(x)), data.frame)
 
 
 
