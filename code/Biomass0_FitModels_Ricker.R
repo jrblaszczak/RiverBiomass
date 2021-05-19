8788d05f226fd35918ac6afe2ea86242008159c5
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2021-05-18 21:38:32 -0700

    Ricker model comparisons

--- code/Biomass0_FitModels_Ricker.R
+++ code/Biomass0_FitModels_Ricker.R
@@ -55,10 +55,10 @@ PM_outputlist_Ricker_Gmod <- lapply(stan_data_l,
                                                 data=x,chains=3,iter=5000,init = init_Ricker,
                                                 control=list(max_treedepth=12)))
 
+
 ###################################
 ## Visualize differences among models
 ##################################
-
 # source simulation models
 source("Predicted_ProductivityModel_Autoregressive.R") # parameters: phi, alpha, beta, sig_p
 source("Predicted_ProductivityModel_Ricker.R") # parameters: r, lambda, s, c, sig_p, sig_o
@@ -66,6 +66,10 @@ source("Predicted_ProductivityModel_Ricker.R") # parameters: r, lambda, s, c, si
 ##########################
 ## Model 1 Output - AR
 #########################
+names(df); names(PM_outputlist_AR)
+AR_list <- Map(c, PM_outputlist_AR, df)
+
+
 AR_sim_fxn <- function(x){
   #separate data
   output <- x[[1]]
@@ -87,12 +91,15 @@ AR_sim_fxn <- function(x){
   
 }
 
-AR_sim <- lapply(PM_outputlist_AR, function(x) AR_sim_fxn(x))
+AR_sim <- lapply(AR_list, function(x) AR_sim_fxn(x))
 
 
 ###############################
 ## Model 2 Output - Ricker
 ###############################
+names(df); names(PM_outputlist_Ricker)
+Ricker_list <- Map(c, PM_outputlist_Ricker, df)
+
 Ricker_sim_fxn <- function(x){
   #separate data
   output <- x[[1]]
@@ -115,13 +122,16 @@ Ricker_sim_fxn <- function(x){
   
 }
 
-Ricker_sim <- lapply(PM_outputlist_Ricker, function(x) Ricker_sim_fxn(x))
+Ricker_sim <- lapply(Ricker_list, function(x) Ricker_sim_fxn(x))
 
 
 
 ###############################
 ## Model 3 Output - Ricker, Lmod
 ###############################
+names(df); names(PM_outputlist_Ricker_Lmod)
+Ricker_list_Lmod <- Map(c, PM_outputlist_Ricker_Lmod, df)
+
 Ricker_sim_fxn_Lmod <- function(x){
   #separate data
   output <- x[[1]]
@@ -144,12 +154,15 @@ Ricker_sim_fxn_Lmod <- function(x){
   
 }
 
-Ricker_sim_Lmod <- lapply(PM_outputlist_Ricker_Lmod, function(x) Ricker_sim_fxn_Lmod(x))
+Ricker_sim_Lmod <- lapply(Ricker_list_Lmod, function(x) Ricker_sim_fxn_Lmod(x))
 
 
 ###############################
 ## Model 4 Output - Ricker, Gmod
 ###############################
+names(df); names(PM_outputlist_Ricker_Gmod)
+Ricker_list_Gmod <- Map(c, PM_outputlist_Ricker_Gmod, df)
+
 Ricker_sim_fxn_Gmod <- function(x){
   #separate data
   output <- x[[1]]
@@ -172,15 +185,133 @@ Ricker_sim_fxn_Gmod <- function(x){
   
 }
 
-Ricker_sim_Gmod <- lapply(PM_outputlist_Ricker_Gmod, function(x) Ricker_sim_fxn_Gmod(x))
+Ricker_sim_Gmod <- lapply(Ricker_list_Gmod, function(x) Ricker_sim_fxn_Gmod(x))
+
+
+## Save prediction
+saveRDS(AR_sim, "./rds files/AR_ws_comparison.rds")
+saveRDS(Ricker_sim, "./rds files/Ricker_ws_comparison.rds")
+saveRDS(Ricker_sim_Lmod, "./rds files/Ricker_Lmod_ws_comparison.rds")
+saveRDS(Ricker_sim_Gmod, "./rds files/Ricker_Gmod_ws_comparison.rds")
+
+#################################
+## Plot within sample pred comparison
+#################################
+
+vis_preds <- function(simmat1_list, mod_title){
+  
+  # For every day extract median and CI
+  median_simmat1 <- ldply(lapply(simmat1_list, function(z) apply(z[[1]], 1, function(x) median(x))), data.frame)
+  lower_simmat1 <- ldply(lapply(simmat1_list, function(z) apply(z[[1]], 1, function(x) quantile(x, probs = 0.025))), data.frame)
+  upper_simmat1 <- ldply(lapply(simmat1_list, function(z) apply(z[[1]], 1, function(x) quantile(x, probs = 0.975))), data.frame)
+  
+  ## Plot simulated GPP
+  dat1 <- ldply(df, data.frame)
+  df_sim1 <- as.data.frame(cbind(dat1$site_name, as.character(dat1$date), dat1$GPP, median_simmat1$X..i.., lower_simmat1$X..i.., upper_simmat1$X..i..))
+  colnames(df_sim1) <- c("site_name","Date","GPP","sim_GPP","sim_GPP_lower","sim_GPP_upper")
+  df_sim1$Date <- as.POSIXct(as.character(df_sim1$Date), format="%Y-%m-%d")
+  df_sim1[,3:6] <- apply(df_sim1[,3:6],2,function(x) as.numeric(as.character(x)))
+  
+  ## Arrange rivers by river order
+  df_sim1 <- left_join(df_sim1, site_info[,c("site_name","short_name")])
+  df_sim1$short_name <- factor(df_sim1$short_name, levels=site_order_list)
+  
+  ## Plot
+  df_sim1_plot <- ggplot(df_sim1, aes(Date, GPP))+
+    geom_point(size=2, color="black")+
+    geom_line(aes(Date, sim_GPP), color="#1C474D", size=1.2)+
+    labs(y=expression('GPP (g '*~O[2]~ m^-2~d^-1*')'), title=mod_title)+
+    geom_ribbon(aes(ymin=sim_GPP_lower,ymax=sim_GPP_upper),
+                fill="#1C474D", alpha=0.3, show.legend = FALSE)+
+    theme(legend.position = "none",
+          panel.background = element_rect(color = "black", fill=NA, size=1),
+          axis.title.x = element_blank(), axis.text = element_text(size=12),
+          axis.title.y = element_text(size=15), axis.text.x = element_text(angle=25, hjust = 1),
+          strip.background = element_rect(fill="white", color="black"),
+          strip.text = element_text(size=15))+
+    coord_cartesian(ylim = c(0,17))+
+    facet_wrap(~short_name, scales = "free", ncol = 2)
+  return(df_sim1_plot)
+  
+}
+
+plot_grid(
+  vis_preds(Ricker_sim, "LB - Ricker"),
+  vis_preds(Ricker_sim_Lmod, "LB - Ricker Light"),
+  vis_preds(Ricker_sim_Gmod, "LB - Ricker GPP"),
+  ncol = 1)
 
 
 
+## Latent biomass
 
+vis_LB_preds <- function(simmat3_list, mod_title){
+  
+  ## Plot latent biomass
+  # For every day extract median and CI
+  median_biomat3 <- ldply(lapply(simmat3_list, function(z) apply(z[[3]], 1, function(x) median(x))), data.frame)
+  lower_biomat3 <- ldply(lapply(simmat3_list, function(z) apply(z[[3]], 1, function(x) quantile(x, probs = 0.025))), data.frame)
+  upper_biomat3 <- ldply(lapply(simmat3_list, function(z) apply(z[[3]], 1, function(x) quantile(x, probs = 0.975))), data.frame)
+  
+  ## Plot simulated GPP
+  dat3 <- ldply(df, data.frame)
+  df_bio3 <- as.data.frame(cbind(dat3$site_name, as.character(dat3$date),
+                                 median_biomat3[,2], lower_biomat3[,2], upper_biomat3[,2]))
+  colnames(df_bio3) <- c("site_name","Date","B","B_lower","B_upper")
+  df_bio3$Date <- as.POSIXct(as.character(df_bio3$Date), format="%Y-%m-%d")
+  df_bio3[,3:5] <- apply(df_bio3[,3:5],2,function(x) as.numeric(as.character(x)))
+  
+  ## Arrange rivers by river order
+  df_bio3 <- left_join(df_bio3, site_info[,c("site_name","short_name")])
+  df_bio3$short_name <- factor(df_bio3$short_name, levels=site_order_list)
+  
+  df_modB3 <- df_bio3
+  
+  ## plot
+  df_modB3_plot <- ggplot(df_modB3, aes(Date, exp(B)))+
+    geom_line(size=1.2, color="chartreuse4")+
+    labs(y="Latent Biomass",title=mod_title)+
+    geom_ribbon(aes(ymin=exp(B_lower),ymax=exp(B_upper)),
+                fill="chartreuse4", alpha=0.3, show.legend = FALSE)+
+    theme(legend.position = "none",
+          panel.background = element_rect(color = "black", fill=NA, size=1),
+          axis.title.x = element_blank(), axis.text = element_text(size=12),
+          axis.title.y = element_text(size=15), axis.text.x = element_text(angle=25, hjust = 1),
+          strip.background = element_rect(fill="white", color="black"),
+          strip.text = element_text(size=15))+
+    coord_cartesian(ylim=c(0,25))+
+    facet_wrap(~short_name, scales = "free_x", ncol = 2)
+  return(df_modB3_plot)
+  
+}
 
+plot_grid(
+  vis_LB_preds(Ricker_sim, "LB - Ricker"),
+  vis_LB_preds(Ricker_sim_Lmod, "LB - Ricker Light"),
+  vis_LB_preds(Ricker_sim_Gmod, "LB - Ricker GPP"),
+  ncol = 1)
 
+##############################
+## Out of sample predictions
+##############################
+## Source data
+source("DataSource_6rivers_oos.R")
 
+dat_oos <- dat_oos[names(df)]
 
+## predict the next year
+# STS
+AR_list_oos <- Map(c, PM_outputlist_AR, dat_oos)
+AR_sim_oos <- lapply(AR_list_oos, function(x) AR_sim_fxn(x))
+# Ricker
+Ricker_list_oos <- Map(c, PM_outputlist_Ricker, dat_oos)
+Ricker_sim_oos <- lapply(Ricker_list_oos, function(x) Ricker_sim_fxn(x))
+# Ricker Lmod
+Ricker_list_Lmod_oos <- Map(c, PM_outputlist_Ricker, dat_oos)
+Ricker_sim_Lmod_oos <- lapply(Ricker_list_Lmod_oos, function(x) Ricker_sim_fxn_Lmod(x))
+# Ricker Gmod
+Ricker_list_Gmod_oos <- Map(c, PM_outputlist_Ricker, dat_oos)
+Ricker_sim_Gmod_oos <- lapply(Ricker_list_Gmod_oos, function(x) Ricker_sim_fxn_Gmod(x))
 
 
 
