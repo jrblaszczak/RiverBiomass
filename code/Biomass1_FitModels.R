d2d2af00223e1e1c9f295308279cf7861cdc847f
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2021-05-21 17:33:19 -0700

    Updated models

--- code/Biomass1_FitModels.R
+++ code/Biomass1_FitModels.R
@@ -7,7 +7,7 @@ lapply(c("plyr","dplyr","ggplot2","cowplot","lubridate",
          "bayesplot","shinystan","Metrics","MCMCglmm","tictoc"), require, character.only=T)
 
 ## Source data
-source("DataSource_6rivers.R")
+source("DataSource_6rivers_StreamLight.R")
 
 ####################
 ## Stan data prep ##
@@ -61,8 +61,8 @@ PM_outputlist_AR <- lapply(stan_data_l,
                            function(x) rstan::stan("Stan_ProductivityModel1_Autoregressive_obserr.stan",
                                                    data=x,chains=3,iter=5000, control=list(max_treedepth=12))) #5000 seconds
 PM_AR_elapsedtime <- lapply(PM_outputlist_AR, function(x) return(get_elapsed_time(x)))
-saveRDS(PM_outputlist_AR, "./rds files/stan_6riv_output_AR_2021_05_17.rds")
-saveRDS(PM_AR_elapsedtime, "./rds files/stan_6riv_AR_time_2021_05_17.rds")
+saveRDS(PM_outputlist_AR, "./rds files/stan_6riv_output_AR_2021_05_21.rds")
+saveRDS(PM_AR_elapsedtime, "./rds files/stan_6riv_AR_time_2021_05_21.rds")
 
 ## PM 2 - Latent Biomass (Ricker)
 init_Ricker <- function(...) {
@@ -72,10 +72,10 @@ init_Ricker <- function(...) {
 PM_outputlist_Ricker <- lapply(stan_data_l,
                                function(x) stan("Stan_ProductivityModel2_Ricker_fixedinit_obserr.stan",
                                                 data=x,chains=3,iter=5000,init = init_Ricker,
-                                                control=list(max_treedepth=12)))
+                                                control=list(max_treedepth=12, adapt_delta = 0.95)))
 PM_Ricker_elapsedtime <- lapply(PM_outputlist_Ricker, function(x) return(get_elapsed_time(x)))
-saveRDS(PM_outputlist_Ricker, "./rds files/stan_6riv_output_Ricker_2021_05_17.rds")
-saveRDS(PM_Ricker_elapsedtime, "./rds files/stan_6riv_Ricker_time_2021_05_17.rds")
+saveRDS(PM_outputlist_Ricker, "./rds files/stan_6riv_output_Ricker_2021_05_21.rds")
+saveRDS(PM_Ricker_elapsedtime, "./rds files/stan_6riv_Ricker_time_2021_05_21.rds")
 
 ## PM 3 - Latent Biomass (Gompertz)
 init_Gompertz <- function(...) {
@@ -87,9 +87,10 @@ PM_outputlist_Gompertz <- lapply(stan_data_l,
                                                   data=x,chains=3,iter=5000, 
                                                   control=list(max_treedepth=12)))
 PM_Gompertz_elapsedtime <- lapply(PM_outputlist_Gompertz, function(x) return(get_elapsed_time(x)))
-saveRDS(PM_outputlist_Gompertz, "./rds files/stan_6riv_output_Gompertz_2021_05_16.rds")
-saveRDS(PM_Gompertz_elapsedtime, "./rds files/stan_6riv_Gompertz_time_2021_05_16.rds")
+saveRDS(PM_outputlist_Gompertz, "./rds files/stan_6riv_output_Gompertz_2021_05_21.rds")
+saveRDS(PM_Gompertz_elapsedtime, "./rds files/stan_6riv_Gompertz_time_2021_05_21.rds")
 
 
-
+## View
+launch_shinystan(PM_outputlist_Ricker$nwis_01608500)
 
