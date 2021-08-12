61b5d81d30851b9e7f91f7506acbef2c47aaf265
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2021-08-12 12:34:24 -0700

    Adjust Data Source scripts for light modifications

--- code/DataSource_6rivers_oos_StreamLight.R
+++ code/DataSource_6rivers_oos_StreamLight.R
@@ -11,6 +11,13 @@ data$date <- as.POSIXct(as.character(data$date), format="%Y-%m-%d")
 
 site_info <- readRDS("./rds files/NWIS_6siteinfo_subset_SL.rds")
 
+# Read in StreamLight processed data (Savoy)
+SL <- readRDS("./rds files/StreamLight_daily_6riv_all.rds")
+colnames(SL)[colnames(SL) == "Date"] <- "date"
+
+## Join data and StreamLight
+data <- left_join(data, SL, by=c("site_name", "date"))
+
 ## Change river names to short names
 site_info$short_name <- revalue(as.character(site_info$site_name), replace = c("nwis_02336526"="Proctor Creek, GA",
                                                                                "nwis_01649190"="Paint Branch, MD",
@@ -60,14 +67,14 @@ post_dat <- subset(data, site_year %in% prev_post$site_year2)
 
 ## extract first year max light and discharge by site
 prev_max <- prev_dat %>% group_by(site_name) %>%
-  summarise_at(.vars = c("Q","light"), .funs = max)
+  summarise_at(.vars = c("Q","PAR_surface"), .funs = max)
 
 oos_relativize <- function(prev_max, post_dat, id){
   
   max.vals <- prev_max[which(prev_max$site_name == id),]
   dat <- post_dat[which(post_dat$site_name == id),]
   
-  dat$light_rel <- dat$light/max.vals$light
+  dat$light_rel <- dat$PAR_surface/max.vals$PAR_surface
   dat$tQ <- dat$Q/max.vals$Q
   
   dat <- dat[order(dat$date),]
@@ -81,6 +88,5 @@ for(i in 1:nrow(prev_max)){
   dat_oos[[i]] <- oos_relativize(prev_max, post_dat, prev_max[i,]$site_name)
 }
 names(dat_oos) <- prev_max$site_name
-
 
 rm(data, data_siteyears, prev_max, prev_dat, post_dat, prev_post, i, oos_relativize)
