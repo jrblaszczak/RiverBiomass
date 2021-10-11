d3640a8c7527ceb857976fcbfe6471a50c6d2765
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2021-10-11 11:49:48 -0700

    Update Biomass0_Simulate_Recover.R

--- code/Biomass0_Simulate_Recover.R
+++ code/Biomass0_Simulate_Recover.R
@@ -3,6 +3,78 @@
 ## Code author: J.R. Blaszczak
 ##==============================================================================
 
+# load packages
+lapply(c("plyr","dplyr","ggplot2","cowplot","lubridate",
+         "parallel","tidyverse","rstan","devtools","shinystan",
+         "MCMCglmm"), require, character.only=T)
+
+## Source data
+source("DataSource_6rivers_StreamLight.R")
+
+## Choose example data
+ex <- df$nwis_01608500
+
+ggplot(ex, aes(date, GPP))+
+  geom_point(color="chartreuse4", size=2)+
+  geom_errorbar(aes(ymin = GPP.lower, ymax = GPP.upper), width=0.2,color="darkolivegreen4")+
+  labs(y=expression('GPP (g '*~O[2]~ m^-2~d^-1*')'), title=df$short_name[1])+
+  theme(legend.position = "none",
+        panel.background = element_rect(color = "black", fill=NA, size=1),
+        axis.title.x = element_blank(), axis.text.x = element_blank(),
+        axis.text.y = element_text(size=12),
+        axis.title.y = element_text(size=12))
+
+## Simulate data using deterministic function
+PM_Ricker <- function(r, lambda, s, c, sig_p, sig_o, df) {
+  
+  ## Data
+  Ndays<-length(df$GPP)
+  GPP <- df$GPP
+  GPP_sd <- df$GPP_sd
+  light <- df$light_rel
+  tQ <- df$tQ # discharge standardized to max value
+  
+  ## Vectors for model output of P, B, pred_GPP
+  P <- numeric(Ndays)
+  P[1] <- 1
+  for(i in 2:length(tQ)){
+    P[i] = exp(-exp(s*(tQ[i] - c)))
+  }
+  
+  B<-numeric(Ndays)
+  B[1] <- log(GPP[1]/light[1])
+  pred_GPP<-numeric(Ndays)
+  pred_GPP[1] <- light[1]*exp(B[1])
+  
+  ## Process Model
+  for (j in 2:Ndays){
+    B[j] <- MCMCglmm::rtnorm(1, mean = (B[j-1] + r + lambda*exp(B[j-1]))*P[j], sd = sig_p, upper = 5)
+  }
+  
+  for (i in 2:Ndays){
+    pred_GPP[i] <- MCMCglmm::rtnorm(1, mean = light[i]*exp(B[i]), sd = sig_o, lower=0.01)
+  }
+  
+  return(pred_GPP)
+}
+
+## Predict
+ex_pred.GPP <- PM_Ricker(r = 0.3, lambda = -0.03,
+                         s = 124, c = 0.28,
+                         sig_p = 0.21, sig_o = 0.9,
+                         df = ex)
+
+## Plot
+
+
+
+
+
+
+
+
+
+
 
 
 
