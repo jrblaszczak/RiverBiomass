9d77899f5dfc775cc2db32f6e92f641f3c450ae3
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2022-02-01 11:15:13 -0800

    Centralized script for result section values

---
+++ code/Biomass00_ResultsValues.R
@@ -0,0 +1,20 @@
+##==============================================================================
+## Script for extracting information for results
+## Code author: J.R. Blaszczak
+##==============================================================================
+
+# load packages
+lapply(c("plyr","dplyr","ggplot2","cowplot","lubridate","parallel",
+         "tidyverse","rstan","bayesplot","shinystan","Metrics","MCMCglmm"), require, character.only=T)
+
+######################
+## S-TS description
+######################
+
+
+
+
+
+
+
+
