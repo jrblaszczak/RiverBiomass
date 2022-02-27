738218641b9ad2084ee0c7698f515e2c5ca70966
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2022-02-27 08:20:04 -0800

    colors for figure

---
+++ code/colors.R
@@ -0,0 +1,6 @@
+library(wesanderson)
+wes_palette("GrandBudapest1")
+wes_palette("Moonrise2")
+
+plot(1,1,col=wes_palette("Moonrise2")[1], pch=19, cex=10)
+points(1.2,1,col=wes_palette("Moonrise2")[2], pch=19, cex=10)
